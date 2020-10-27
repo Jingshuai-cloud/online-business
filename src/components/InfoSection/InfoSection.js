172626f0db2175b9ab6dd215394667c36d0b4fe6
Jingshuai-cloud <xujingshuai@gmail.com>
2020-10-27 23:44:35 +1100

    initial commit

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  InfoSec,
+  InfoRow,
+  InfoColumn,
+  TextWrapper,
+  TopLine,
+  Heading,
+  SubTitle,
+  ImgWrapper,
+  Img,
+} from "./InfoSection.elements";
+import { Container, Button } from "../../globalStyle";
+const InfoSection = ({
+  primary,
+  lightBg,
+  imgStart,
+  lightTextDesc,
+  lightTopLine,
+  buttonLabel,
+  description,
+  headLine,
+  lightText,
+  topLine,
+  img,
+  alt,
+  start,
+}) => {
+  return (
+    <>
+      <InfoSec lightBg={lightBg}>
+        <Container>
+          <InfoRow imgStart={imgStart}>
+            <InfoColumn>
+              <TextWrapper>
+                <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headLine}</Heading>
+                <SubTitle lightTextDesc={lightTextDesc}>{description}</SubTitle>
+                <Link to="/sign-up">
+                  <Button big fontBig primary={primary}>
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </TextWrapper>
+            </InfoColumn>
+            <InfoColumn>
+              <ImgWrapper start={start}>
+                <Img src={img} alt={alt} />
+              </ImgWrapper>
+            </InfoColumn>
+          </InfoRow>
+        </Container>
+      </InfoSec>
+    </>
+  );
+};
+
+export default InfoSection;
